82cf97532bc68db9412bd73782174f64489d203e
TE24B-Mia-Hindersson <mia@hindersson.com>
2025-11-18 16:15:31 +0100

    behöver fixa difficulty

--- projekt/projekt/Program.cs
+++ projekt/projekt/Program.cs
@@ -1,31 +1,54 @@
-﻿Console.WriteLine("Welcome to ... pet adoption!");
+﻿Console.WriteLine("Welcome to Stardust pet adoption!");
 Console.WriteLine("Please tell me which, between these lovely three pets you would like?");
 Console.WriteLine("1. Duck (difficulty easy)");
 Console.WriteLine("2. Frog (difficulty medium)");
 Console.WriteLine("3. Cat (difficulty hard)");
+Console.WriteLine("Enter the number of your choice:");
 string pet = Console.ReadLine();
 int choicep;
-int.TryParse(pet, out choicep);
+bool isvalidchoice = int.TryParse(pet, out choicep);
 int difficulty = 10;
+string diff;
 Console.Clear();
 Console.ReadLine();
-if (choicep == 1)
+if (!isvalidchoice || choicep < 1 || choicep > 3 )
+{
+   Console.WriteLine("Please choose a valid pet!!"); 
+}
+ else if ( choicep == 1)
 {
     pet = "Duck";
+    difficulty = 10;
 }
-else if (choicep == 2)
+else if ( choicep == 2)
 {
     pet = "Frog";
-    difficulty += 10;
+    difficulty = 20;
 }
 else if (choicep == 3)
 {
     pet = "Cat";
-     difficulty += 20;
+     difficulty = 30;
+}
+if (difficulty == 10)
+{
+    diff = "easy";
+}
+if (difficulty == 20)
+{
+    diff = "medium";
+}
+if (difficulty == 30)
+{
+    diff = "hard";
+}
+else
+{
+    diff = "unknown";
 }
 Console.WriteLine("Excellent choice!");
 Console.WriteLine($"-you now own a {pet}-");
-Console.WriteLine($"-{difficulty}-");
+Console.WriteLine($"- difficulty level:{diff}-");
 
 
 
